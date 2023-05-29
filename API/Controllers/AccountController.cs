5177d2003911990995c05c487241ecd477efe240
Fernando Falcone <C0647451@vale.com>
2023-05-29 15:42:51 -0300

    Adds Account Controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] // api/account/register
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.UserName)) return BadRequest("Username has already been taken");
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
