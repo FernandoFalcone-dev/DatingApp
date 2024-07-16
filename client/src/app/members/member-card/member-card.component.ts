013d4033c939b55b11df36c640114871de7aacbf
Fernando Falcone <C0647451@vale.com>
2024-07-16 16:44:15 -0300

    feat: adds member card and animations

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  @Input() member: Member | undefined;
+  
+  ngOnInit(): void {
+  }
+
+}
